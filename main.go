b94f5c297237afb048a0e52b29cc0abfa2775a42
Gilbert Aurel <aurel.gilbert10@gmail.com>
2021-11-27 20:38:03 +0700

    feat: learn about variable and types

--- main.go
+++ main.go
@@ -6,5 +6,18 @@ import (
 
 func main() {
 	var name string = "golang"
+	var number1 int8 = 7
+	var number2 int8 = 3
+	var result float32 = float32(number1) / float32(number2)
+
 	fmt.Println(name)
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Printf("1+1 = %v \n", 1+1)
+
+	fmt.Println(result)
+
+	fmt.Println(true)
+	fmt.Println(number1 < number2)
+	fmt.Println(!(false || true))
 }
